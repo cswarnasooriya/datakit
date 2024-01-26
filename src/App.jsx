8a20317a6ceda2a3610c47f99949b13c541c6219
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-01-26 12:02:44 +0530

    Hero Section Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from './components/Navbar';
 import './App.css';
+import Hero from './components/Hero';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Hero/>
         
     </>
   )
